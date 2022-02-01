cbd448c1c7c95ace8b8570809c2eb960bc1e94a7
takashi247 <takashi247@gmail.com>
2022-02-01 21:03:46 +0900

    add ex00

---
+++ ex00/include/Bureaucrat.hpp
@@ -0,0 +1,37 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+ public:
+  static const int kHighestGrade = 1;
+  static const int kLowestGrade = 150;
+  Bureaucrat();
+  Bureaucrat(const std::string name, int grade);
+  Bureaucrat(const Bureaucrat &other);
+  Bureaucrat &operator=(const Bureaucrat &other);
+  ~Bureaucrat();
+  const std::string &getName() const;
+  int getGrade() const;
+  void getPromoted();
+  void getDemoted();
+
+  class GradeTooHighException : public std::exception {
+    virtual const char *what() const throw();
+    static const char* kErrMsgGradeTooHigh;
+  };
+
+  class GradeTooLowException : public std::exception {
+    virtual const char *what() const throw();
+    static const char* kErrMsgGradeTooLow;
+  };
+
+ private:
+  const std::string name_;
+  int grade_;
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &b);
+
+#endif // BUREAUCRAT_HPP
---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,61 @@
+#include "Bureaucrat.hpp"
+
+#include <iostream>
+
+int main(void)
+{
+  Bureaucrat john("John", 100);
+
+  std::cout << john << std::endl;
+
+  // get promoted
+  john.getPromoted();
+  std::cout << john << std::endl;
+
+  // get demoted
+  john.getDemoted();
+  std::cout << john << std::endl;
+
+  // test error handling
+  Bureaucrat vice_minister("Vice Minister", 1);
+  Bureaucrat intern("Intern", 150);
+  std::cout << vice_minister << std::endl;
+  std::cout << intern << std::endl;
+
+  try {
+    vice_minister.getPromoted();
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    intern.getDemoted();
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  // test constructor error handling
+  try {
+    Bureaucrat no_name;
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    Bureaucrat too_low("too low", 200);
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    Bureaucrat too_high("too high", -200);
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
+
+#ifdef LEAKS
+void	detect_leak(void)__attribute__((destructor));
+
+void	detect_leak(void)
+{
+  system("leaks -q a.out");
+}
+#endif
