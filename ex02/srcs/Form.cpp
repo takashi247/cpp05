930535d16c990f1585695f9fb6f5f35c6b9e5ddd
takashi247 <takashi247@gmail.com>
2022-02-02 14:34:54 +0900

    copy ex01 to ex02

---
+++ ex02/srcs/Form.cpp
@@ -0,0 +1,59 @@
+#include "../include/Form.hpp"
+
+#include <iostream>
+
+#include "../include/Bureaucrat.hpp"
+
+const char* Form::GradeTooHighException::kErrMsgGradeTooHigh =
+"Error: Form: Grade is too high";
+
+const char* Form::GradeTooLowException::kErrMsgGradeTooLow =
+"Error: Form: Grade is too low";
+
+Form::Form(const std::string name, const int grade_to_sign, const int grade_to_execute)
+    : name_(name),
+      is_signed_(false),
+      grade_to_sign_(grade_to_sign),
+      grade_to_execute_(grade_to_execute) {
+  // std::cout << "Form " << name_ << "'s constructor called" << std::endl;
+  if (grade_to_sign_ < Bureaucrat::kHighestGrade || grade_to_execute_ < Bureaucrat::kHighestGrade) {
+    throw Form::GradeTooHighException();
+  } else if (Bureaucrat::kLowestGrade < grade_to_sign_ || Bureaucrat::kLowestGrade < grade_to_execute_) {
+    throw Form::GradeTooLowException();
+  }
+}
+
+Form::~Form() {
+  // std::cout << "Form's destructor called" << std::endl;
+}
+
+const std::string &Form::getName() const { return name_; }
+
+const bool &Form::checkIfSigned() const { return is_signed_; }
+
+int Form::getGradeToSign() const { return grade_to_sign_; }
+
+int Form::getGradeToExecute() const { return grade_to_execute_; }
+
+void Form::beSigned(Bureaucrat &b) {
+  if (grade_to_sign_ < b.getGrade()) {
+    throw Form::GradeTooLowException();
+  } else {
+    is_signed_ = true;
+  }
+}
+
+const char *Form::GradeTooHighException::what() const throw() {
+  return Form::GradeTooHighException::kErrMsgGradeTooHigh;
+}
+
+const char *Form::GradeTooLowException::what() const throw() {
+  return Form::GradeTooLowException::kErrMsgGradeTooLow;
+}
+
+std::ostream &operator<<(std::ostream &os, const Form &f) {
+  os << "Form name: " << f.getName() << "\nIs singed?: " << std::boolalpha
+  << f.checkIfSigned() << "\nMin. grade to sign: " << f.getGradeToSign()
+  << "\nMin. grade to execute: " << f.getGradeToExecute();
+  return (os);
+}
---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,52 @@
+#include "../include/Bureaucrat.hpp"
+#include "../include/Form.hpp"
+
+#include <iostream>
+
+int main(void)
+{
+  Bureaucrat john("John", 100);
+
+  std::cout << john << std::endl;
+
+  Form form("simple form", 99, 99);
+
+  std::cout << std::endl;
+  std::cout << form << std::endl;
+  std::cout << std::endl;
+
+  // test exception handling for form
+  john.signForm(form);
+
+  // get promoted
+  john.getPromoted();
+  std::cout << john << std::endl;
+
+  // test successful signs
+  john.signForm(form);
+
+  std::cout << std::endl;
+  std::cout << form << std::endl;
+  std::cout << std::endl;
+
+  // test error handling for Form
+  try {
+    Form too_high("too high", -100, 100);
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    Form too_low("too low", 100, 300);
+  } catch (std::exception const &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
+
+#ifdef LEAKS
+void	detect_leak(void)__attribute__((destructor));
+
+void	detect_leak(void)
+{
+  system("leaks -q a.out");
+}
+#endif
